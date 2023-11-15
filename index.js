4e1a577c00562bf4bfde2c33c667ed94d371a8d5
Abhishek <abhisheksrivastava199924@gmail.com>
2023-11-15 11:27:18 +0530

    fixed the input size issue and implemented all calculation functionality

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
-// const input=document.getElementsByName("button");
-
-
-// for(const button of input){
-//     button.addEventListener("click",function(event){
-//      const display= event.target.innerHTML;
-//     if(display!=='C' && display!=='AC' && display!=='='){
-//      document.getElementById("display").value += display;
-//     }
-
-// });
-// }
-
-
 
 document.getElementById("display").value='0';
 function input(value){
     if(value!=='C' && value!=='AC' && value!=='='){
         // entering the input in the input field of id display using buttons of the calculator
+        if(value=='%'){
+            let temp=document.getElementById("display").value;
+            document.getElementById("display").value=eval(temp);
+            document.getElementById("display").value=temp/100;
+        }
+        else{
         document.getElementById("display").value +=value;
+        }
     }
     // erasing all input a time
     else if(value=='AC'){
         document.getElementById("display").value='';
     }
     // below code is erasing one character at a time from right side
-    else if(value=='C' ){
+    else if(value=='C'){
       let txt=document.getElementById("display").value;
       let data= txt[txt.length-1];
       document.getElementById("display").value= txt.replace(data,'');
-    
     }
     else if(value=='='){
         let sum=document.getElementById("display").value;
